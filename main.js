b6629743a5f69408fdff78ae91861b372f1731c8
Robert Teesaar <robertteesaar@gmail.com>
2022-09-16 21:36:50 +0300

    initialcommit

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ const navItems = document.querySelector('.list');
 const headline = document.querySelector('.headline');
 const saunColor = document.querySelector('.span');
 const current = document.querySelector('.accent');
+const buttonOpen = document.querySelector('[data-toggle="open"]');
+const buttonClose = document.querySelector('[data-toggle="close"]');
+const overlayEl = document.querySelector('.nav_overlay');
+const menuEl = document.querySelector('.nav-mobile');
 
 const tl = new TimelineMax();
 
@@ -32,3 +36,14 @@ sr.reveal(`.meist-description`, {
 sr.reveal(`.footer-text, .footer-description, hr, .copyright`, {
   origin: 'bottom'
 })
+
+
+const toggleMenu = () => {
+  menuEl.classList.toggle('active');
+  overlayEl.classList.toggle('active');
+  document.body.classList.toggle('scroll-false');
+}
+
+buttonOpen.addEventListener('click', toggleMenu);
+buttonClose.addEventListener('click', toggleMenu);
+overlayEl.addEventListener('click', toggleMenu);
