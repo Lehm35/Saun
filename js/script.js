85f62c1c2a08ee175b1e3d946112d015d29d9e8e
Robert Teesaar <robertteesaar@gmail.com>
2022-10-12 18:29:15 +0300

    initialcommit

--- js/script.js
+++ js/script.js
@@ -3,6 +3,22 @@ const buttonOpen = document.querySelector('[data-toggle="open"]');
 const buttonClose = document.querySelector('[data-toggle="close"]');
 const overlayEl = document.querySelector('.nav_overlay');
 const menuEl = document.querySelector('.nav-mobile');
+const heroImage = document.querySelector('.hero-img')
+const headline = document.querySelector('.hero-headline');
+
+const tl = new TimelineMax();
+
+tl.fromTo(heroImage, 1, { height: "0%" }, { height: "80%", ease: Power2.easeInOut })
+.fromTo(headline, 1, {opacity: "0"}, {opacity: "1", ease: Power2.easeInOut});
+
+const sr = ScrollReveal({
+  distance: '25px',
+  duration: 2000
+});
+
+sr.reveal(`section`, {
+  origin: 'bottom'
+})
 
 
 const toggleMenu = () => {
